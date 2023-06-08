647efd85267ac71639cf0526a30b1e282a5644e8
Kasjful Kurniawan <kasful.bpp@gmail.com>
2023-06-08 10:31:13 +0800

    feat: user list

--- pages/user-list.tsx
+++ pages/user-list.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StyleSheet, Button, View, Text } from 'react-native';
 import type { UserListProps } from '../types/stacks';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 export default function UserListScreen({ navigation }: UserListProps) {
   const [username,setUsername] = React.useState('');
@@ -21,9 +22,16 @@ export default function UserListScreen({ navigation }: UserListProps) {
         }
         <View style={styles.button}>
           <Button
-            title="Go to Home"
+            title="Go to Hosme"
             onPress={() => navigation.navigate('Home')}
           />
+        </View>
+        <View style={styles.addButton}>
+        <FontAwesomeIcon icon="comments" />
+          <Button
+              title="Add Contact"
+              onPress={() => navigation.navigate('Home')}
+            />
         </View>
       </View>
     );
@@ -45,6 +53,10 @@ const styles = StyleSheet.create({
   button: {
     marginTop: offset,
     marginLeft: offset,
-    fontSize: offset,
+  },
+  addButton: {
+    
+    marginTop: offset,
+    marginLeft: offset,
   }
 });
